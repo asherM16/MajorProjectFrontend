8cfe06a878e9d636c00d332ffcd4284eef34f02c
saklain404 <mohdsaklain8899@gmail.com>
2021-01-04 22:59:30 +0530

    minor changes

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from '../components/layout';
+import { Button, Card } from 'antd';
 
 
 
@@ -49,5 +50,25 @@ export default function AboutUs() {
                 To provide a platform towards the right career and building visionaries and leaders for the nation. To increase the visibility of academic programs globally and attract corporate at all levels.
 
             </p>
+
+            <h2 className="about-h2">Invest in Learning Better</h2>
+            <div className="about-card">
+
+                <Card title={<p className="about-title"> Curriculum Coverage</p>} style={{ width: 300, borderRadius: 10, }}>
+
+                    <p>We have analyzed curriculum coverage pattern of top schools of Delhi NCR and aligned our curriculum coverage of all classes and subjects as per theirs.</p>
+                </Card>
+
+                <Card title={<h2 className="about-title">Build Concepts </h2>} style={{ width: 300, borderRadius: 10,  }}>
+                    <p>Content for each class and subject is designed by 180+ faculty. Individual attention in group environment helps students understand their stregths and areas of improvements.</p>
+                </Card>
+
+                <Card title={<h2 className="about-title">Performance Improvement </h2>} style={{ width: 300, borderRadius: 10,  }}>
+                    
+                    <p>Structured & scheduled progressive tests are conducted to keep regular track of students' learning and understanding of concepts.</p>
+                </Card>
+            </div>
+            <h2 className="about-h2"> Prepare Yourself For a Better Tommorrow... </h2>
+
         </Layout>)
 }
